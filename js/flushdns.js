b82805cf24adf541fac4c11419f063453316d89e
ミドリ <midori01@users.noreply.github.com>
2023-05-22 07:11:08 +0800

    Update flushdns.js

--- js/flushdns.js
+++ js/flushdns.js
@@ -15,7 +15,7 @@
     }
     if ($trigger == "button") await httpAPI("/v1/dns/flush");
     let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
-    panel.content = `DNS Latency: ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`;
+    panel.content = `[DNS Latency] ${delay}ms${dnsCache ? `\nserver:\n${dnsCache}` : ""}`;
     $done(panel);
 })();
 
