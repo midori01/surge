a34793a4316fd3084f47e27dbd41940caaa773a7
ミドリ <midori01@users.noreply.github.com>
2024-08-02 18:16:14 +0800

    Update network.js

--- js/network.js
+++ js/network.js
@@ -113,7 +113,7 @@ function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
         `[Provider] ${info.isp}\n` +
         `[Location] ${info.city}, ${info.country}`,
       icon: getSSID() ? 'wifi' : 'simcard',
-      'icon-color': getSSID() ? '#006AB6' : '#006AB6',
+      'icon-color': getSSID() ? '#73C2FB' : '#73C2FB',
     });
   }).catch(error => {
     if (String(error).startsWith("Network changed")) {
