ea5a6a6e0716be79e51c50409e215fbeb0924604
ミドリ <midori01@users.noreply.github.com>
2024-08-05 14:07:09 +0800

    Update network.js

--- js/network.js
+++ js/network.js
@@ -54,10 +54,11 @@ function getSSID() {
 
 function getIP() {
   const { v4, v6 } = $network;
-  const protocol = v6?.primaryAddress ? 'IPv4/IPv6 Dual Stack' : 'IPv4 Single Stack';
+  const protocol = v6?.primaryAddress ? 'IPv4 & IPv6' : 'IPv4 Only';
+  const interface = v4?.primaryInterface ? `${v4.primaryInterface}` : '';
   const gateway = v4?.primaryRouter ? `[Gateway] ${v4.primaryRouter}` : '';
   const internalIP = v4?.primaryAddress ? `[Internal IP] ${v4.primaryAddress}` : '';
-  return `${!v4 && !v6 ? 'Network Error' : `[Protocol] ${protocol}\n${gateway}\n${internalIP}`}\n`;
+  return `${!v4 && !v6 ? 'Network Error' : `[Protocol] ${protocol} (${interface})\n${gateway}\n${internalIP}`}\n`;
 }
 
 function getSTUNIP() {
