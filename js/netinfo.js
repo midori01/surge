3afe24337d52ddb119c7c3b20defed46fa0dc4eb
ミドリ <midori01@users.noreply.github.com>
2024-11-10 02:27:19 +0900

    Update netinfo.js

--- js/netinfo.js
+++ js/netinfo.js
@@ -1,22 +1,14 @@
 class httpMethod {
-  static _httpRequestCallback(resolve, reject, error, response, data) {
-    error ? reject(error) : resolve({ ...response, data });
-  }
-
   static request(method, option = {}) {
     return new Promise((resolve, reject) => {
       $httpClient[method](option, (error, response, data) => {
-        this._httpRequestCallback(resolve, reject, error, response, data);
+        error ? reject(error) : resolve({ ...response, data });
       });
     });
   }
 
   static get(option = {}) {
     return this.request('get', option);
-  }
-
-  static post(option = {}) {
-    return this.request('post', option);
   }
 }
 
@@ -25,16 +17,15 @@ function randomString32() {
   return Array.from({ length: 32 }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
 }
 
-function getCurrentTimestamp() {
-  const now = new Date();
-  return now.toTimeString().slice(0, 5);
+function getTimestamp() {
+  return new Date().toTimeString().slice(0, 5);
 }
 
 function getProtocolType() {
   return $network.v6?.primaryAddress ? 'Dual-Stack' : 'Single-Stack';
 }
 
-function getCellularInfo() {
+function getNetworkInfoType() {
   const radioGeneration = {
     'GPRS': '2.5G',
     'CDMA1x': '2.5G',
@@ -48,54 +39,53 @@ function getCellularInfo() {
     'eHRPD': '3.9G',
     'LTE': '4G',
     'NRNSA': '5G',
-    'NR': '5G',
+    'NR': '5G'
   };
 
+  const wifiSSID = $network.wifi?.ssid;
   const radio = $network['cellular-data']?.radio;
-  return $network['cellular-data'] && !$network.wifi?.ssid && radio
-    ? `${radioGeneration[radio]} ${radio} | ${getProtocolType()} | ${getCurrentTimestamp()}`
-    : '';
-}
-
-function getSSID() {
-  const ssid = $network.wifi?.ssid || '';
-  return ssid ? `${ssid} | ${getProtocolType()} | ${getCurrentTimestamp()}` : '';
+  if (wifiSSID) return { type: 'WiFi', info: wifiSSID };
+  if (radio) return { type: 'Cellular', info: `${radioGeneration[radio] || ''} ${radio}`.trim() };
+  return { type: 'Unknown', info: '' };
 }
 
 async function resolveHostname(ip) {
-  const ipToReverseDNS = (ip) => {
-    return ip.split('.').reverse().join('.') + '.in-addr.arpa';
-  };
-
-  const reverseDNS = ipToReverseDNS(ip);
-
+  const reverseDNS = ip.split('.').reverse().join('.') + '.in-addr.arpa';
   try {
     const response = await httpMethod.get(`http://223.5.5.5/resolve?name=${reverseDNS}&type=PTR`);
     const data = JSON.parse(response.data);
-    if (data && data.Answer && data.Answer.length > 0) {
-      return data.Answer[0].data;
-    }
+    return data?.Answer?.[0]?.data || 'Lookup Failed: NXDOMAIN';
   } catch (error) {
     console.error('Error resolving hostname:', error);
+    return 'Lookup Failed: NXDOMAIN';
   }
-  return 'Lookup Failed: NXDOMAIN';
 }
 
 async function fetchNetworkData() {
-  const ipApiResponse = await httpMethod.get('http://208.95.112.1/json');
-  const dnsApiResponse = await httpMethod.get(`http://${randomString32()}.edns.ip-api.com/json`);
+  return Promise.all([
+    httpMethod.get('http://208.95.112.1/json'),
+    httpMethod.get(`http://${randomString32()}.edns.ip-api.com/json`)
+  ]);
+}
 
-  return { ipApiResponse, dnsApiResponse };
+async function retryOperation(fn, retries, delay) {
+  let attempts = 0;
+  while (attempts < retries) {
+    try {
+      return await fn();
+    } catch (error) {
+      attempts++;
+      if (attempts < retries) {
+        console.warn(`Retrying... Attempt ${attempts}`);
+        await new Promise(resolve => setTimeout(resolve, delay));
+      } else {
+        throw error;
+      }
+    }
+  }
 }
 
 async function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
-  const checkStatus = (response) => {
-    if (response.status > 300) {
-      throw new Error(`Request error with HTTP status code: ${response.status}\n${response.data}`);
-    }
-    return response;
-  };
-
   const dnsGeoMap = {
     "Japan - NTT": "Japan - NTT Corp",
     "Japan - KDDI": "Japan - KDDI Corp.",
@@ -182,76 +172,59 @@ async function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
     "China - China Broadband": "China - China Broadband"
   };
 
+  const locationMap = {
+    'GB': (info) => `${info.city}, ${info.regionName}, UK`,
+    'AE': (info) => `${info.city}, ${info.region}, UAE`,
+    'TW': (info) => /Taipei/.test(info.city) ? `Taipei, ROC (${info.country})` : `${info.city}, ROC (${info.country})`,
+    'CN': (info) => ['Beijing', 'Shanghai', 'Tianjin', 'Chongqing'].includes(info.regionName) 
+          ? `${info.regionName}, PRC` 
+          : `${info.city}, ${info.regionName}, PRC`,
+    'JP': (info) => (info.regionName === 'Tokyo' && info.city === 'Tokyo') 
+          ? `${info.regionName}, ${info.country}`
+          : `${info.city}, ${info.regionName}, ${info.country}`,
+    'US': (info) => `${info.city}, ${info.region}, USA`,
+    'CA': (info) => `${info.city}, ${info.region}, ${info.country}`,
+    'HK': (info) => `${info.country} SAR, PRC`,
+    'MO': (info) => `${info.country} SAR, PRC`,
+    'KR': (info) => {
+      let city = info.city.split('-')[0];
+      return `${city}, ${info.country}`;
+    },
+    'SG': (info) => `${info.country} (${info.countryCode})`,
+    'VA': (info) => `${info.country} (${info.countryCode})`,
+    'MC': (info) => `${info.country} (${info.countryCode})`,
+    'GI': (info) => `${info.country} (${info.countryCode})`,
+    'default': (info) => `${info.city}, ${info.country}`
+  };
+
   while (retryTimes > 0) {
     try {
-      const { ipApiResponse, dnsApiResponse } = await fetchNetworkData();
-
-      checkStatus(ipApiResponse);
-      checkStatus(dnsApiResponse);
-
-      const ipApiInfo = JSON.parse(ipApiResponse.data);
-      const hostname = await resolveHostname(ipApiInfo.query);
-      const dnsApiInfo = JSON.parse(dnsApiResponse.data).dns;
-
-      let dnsLeakInfo;
-      const matchedKey = Object.keys(dnsGeoMap).find(key => dnsApiInfo.geo.toLowerCase().includes(key.toLowerCase()));
-      dnsLeakInfo = matchedKey ? dnsGeoMap[matchedKey] : dnsApiInfo.geo;
-
-      let location;
-      if (ipApiInfo.countryCode === 'GB') {
-        location = `${ipApiInfo.city}, ${ipApiInfo.regionName}, UK`;
-      } else if (ipApiInfo.countryCode === 'AE') {
-        location = `${ipApiInfo.city}, ${ipApiInfo.region}, UAE`;
-      } else if (ipApiInfo.city === 'Johannesburg') {
-        location = `Joburg, ${ipApiInfo.country}`;
-      } else if (ipApiInfo.city === 'Frankfurt am Main') {
-        location = `Frankfurt, ${ipApiInfo.country}`;
-      } else if (ipApiInfo.countryCode === 'TW') {
-        const isTaipei = /Taipei/.test(ipApiInfo.regionName) || /Taipei/.test(ipApiInfo.city);
-        location = isTaipei ? `Taipei, ROC (${ipApiInfo.country})` : `${ipApiInfo.city}, ROC (${ipApiInfo.country})`;
-      } else if (ipApiInfo.countryCode === 'CN') {
-        if (['Beijing', 'Shanghai', 'Tianjin', 'Chongqing'].includes(ipApiInfo.regionName)) {
-          location = `${ipApiInfo.regionName}, P.R. China`;
-        } else {
-          location = `${ipApiInfo.city}, ${ipApiInfo.regionName}, PRC`;
-        }
-      } else if (ipApiInfo.countryCode === 'JP') {
-        if (ipApiInfo.regionName === 'Tokyo' && ipApiInfo.city === 'Tokyo') {
-          location = `${ipApiInfo.regionName}, ${ipApiInfo.country}`;
-        } else {
-          location = `${ipApiInfo.city}, ${ipApiInfo.regionName}, ${ipApiInfo.country}`;
-        }
-      } else if (ipApiInfo.countryCode === 'US') {
-        location = `${ipApiInfo.city}, ${ipApiInfo.region}, USA`;
-      } else if (ipApiInfo.countryCode === 'CA') {
-        location = `${ipApiInfo.city}, ${ipApiInfo.region}, ${ipApiInfo.country}`;
-      } else if (['HK', 'MO'].includes(ipApiInfo.countryCode)) {
-        location = `${ipApiInfo.country} SAR, PRC`;
-      } else if (['SG', 'VA', 'MC', 'GI'].includes(ipApiInfo.countryCode)) {
-        location = `${ipApiInfo.country} (${ipApiInfo.countryCode})`;
-      } else if (ipApiInfo.countryCode === 'KR') {
-        let city = ipApiInfo.city;
-        if (city.includes('-')) {
-          city = city.split('-')[0];
-        }
-        location = `${city}, ${ipApiInfo.country}`;
-      } else {
-        location = `${ipApiInfo.city}, ${ipApiInfo.country}`;
-      }
+      const [ipApiResponse, dnsApiResponse] = await retryOperation(fetchNetworkData, retryTimes, retryInterval);
+      if (ipApiResponse.status > 300) throw new Error(`HTTP status code: ${ipApiResponse.status}`);
+      if (dnsApiResponse.status > 300) throw new Error(`HTTP status code: ${dnsApiResponse.status}`);    
+      const infoType = getNetworkInfoType();
+      const title = `${infoType.info} | ${getProtocolType()} | ${getTimestamp()}`;  
+      const ipInfo = JSON.parse(ipApiResponse.data);
+      const hostname = await resolveHostname(ipInfo.query);
+      const dnsGeo = JSON.parse(dnsApiResponse.data).dns.geo;    
+      let dnsLeakInfo = Object.keys(dnsGeoMap).find(keyword => dnsGeo.toLowerCase().includes(keyword.toLowerCase())) 
+        ? dnsGeoMap[Object.keys(dnsGeoMap).find(keyword => dnsGeo.toLowerCase().includes(keyword.toLowerCase()))] 
+        : dnsGeo;
+      const location = locationMap[ipInfo.countryCode] 
+        ? locationMap[ipInfo.countryCode](ipInfo) 
+        : locationMap['default'](ipInfo);
 
       $done({
-        title: getSSID() ? `${getSSID()}` : getCellularInfo(),
-        content: `IP Address: ${ipApiInfo.query}\nPTR: ${hostname}\nISP: ${ipApiInfo.as}\nLocation: ${location}\nDNS Leak: ${dnsLeakInfo}`,
-        icon: getSSID() ? 'wifi' : 'simcard',
+        title,
+        content: `IP Address: ${ipInfo.query}\nPTR: ${hostname}\nISP: ${ipInfo.as}\nLocation: ${location}\nDNS Leak: ${dnsLeakInfo}`,
+        icon: infoType.type === 'WiFi' ? 'wifi' : 'simcard',
         'icon-color': '#73C2FB',
       });
-
       return;
     } catch (error) {
+      console.error(`Attempt failed, retries left: ${retryTimes - 1}`, error);
       retryTimes--;
-      if (retryTimes > 0) {
-        await new Promise(resolve => setTimeout(resolve, retryInterval));
-      }
+      if (retryTimes > 0) await new Promise(resolve => setTimeout(resolve, retryInterval));
     }
   }
 
@@ -264,11 +237,6 @@ async function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
 }
 
 (() => {
-  const retryTimes = 5;
-  const retryInterval = 1000;
-  const surgeMaxTimeout = 29500;
-  const scriptTimeout = Math.min(retryTimes * 5000 + retryTimes * retryInterval, surgeMaxTimeout);
-
   const timeoutHandle = setTimeout(() => {
     $done({
       title: "Timeout",
@@ -276,7 +244,7 @@ async function getNetworkInfo(retryTimes = 5, retryInterval = 1000) {
       icon: 'wifi.exclamationmark',
       'icon-color': '#CB1B45',
     });
-  }, scriptTimeout);
-
-  getNetworkInfo(retryTimes, retryInterval).finally(() => clearTimeout(timeoutHandle));
+  }, 29500);
+  
+  getNetworkInfo().finally(() => clearTimeout(timeoutHandle));
 })();
