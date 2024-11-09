f1b07b993adc0946ba1a5bd633f1c5ee7380910e
ミドリ <midori01@users.noreply.github.com>
2024-11-09 17:42:06 +0900

    Update serverinfo.js

--- js/serverinfo.js
+++ js/serverinfo.js
@@ -21,7 +21,7 @@
       title: params.name || `${hostname} | ${timeString}`,
       icon: params.icon || 'aqi.medium',
       "icon-color": getColorBasedOnMemUsage(parseInt(jsonData.mem_usage)),
-      content: `Status: CPU ${cpuUsage} | MEM ${memUsage}\nTraffic: RX ${bytesToSize(outTraffic)} | TX ${bytesToSize(inTraffic)}\nUptime: ${uptime}`
+      content: `Status: CPU ${cpuUsage} | MEM ${memUsage}\nTraffic: ↓ ${bytesToSize(outTraffic)} | ↑ ${bytesToSize(inTraffic)}\nUptime: ${uptime}`
     };
 
     $done(panel);
