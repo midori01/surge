4f3473cabb3dad1f7833a4dd1a9d104180e4a4bb
ミドリ <midori01@users.noreply.github.com>
2024-11-15 21:00:09 +0900

    Update spotify.request.js

--- js/spotify.request.js
+++ js/spotify.request.js
@@ -1,7 +1,16 @@
 let url = $request.url;
-if (url.includes('platform=iphone')) {
-    url = url.replace(/platform=iphone/, 'platform=ipad');
+if (url.match(/^https:\/\/spclient\.wg\.spotify\.com\/user-customization-service\/v1\/customize$/)) {
+    let headers = $request.headers;
+    if (headers['if-none-match']) {
+        delete headers['if-none-match'];
+    }
+    $done({
+        url,
+        headers
+    });
+} else {
+    if (url.includes('platform=iphone')) {
+        url = url.replace(/platform=iphone/, 'platform=ipad');
+    }
+    $done({ url });
 }
-$done({
-    url
-});
