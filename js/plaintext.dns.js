185109e1ad11ab32ec5fc3b45d124f3779bfcbcf
ミドリ <midori01@users.noreply.github.com>
2024-11-16 18:52:33 +0900

    Delete js/plaintext.dns.js

--- js/plaintext.dns.js
+++
@@ -1,7 +0,0 @@
-$httpClient.get('http://119.29.29.29/d?dn=' + $domain, function(error, response, data){
-  if (error) {
-    $done({});
-  } else {
-$done({addresses: data.split(';'), ttl: 600});
-  }
-});
